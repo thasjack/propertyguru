060b8672da14318a1d9530ccc7d0a11e00175dac
thas <thas@test1.com>
2012-11-24 18:55:43 +0800

    Integer Validation

--- propertyguru/Validate.php
+++ propertyguru/Validate.php
@@ -1,10 +1,35 @@
 <?php
 Class Validate
 {
-	public $validateVariable = null;
+	protected $validateVariable = null;
 	
+	/**
+	 * Set Validate Variable.
+	 * This Will Trimmed the variable
+	 * @param string $var
+	 */
+	public function setValidateVariable($var)
+	{
+		$this->validateVariable = trim($var);
+	}
+	
+	/**
+	 * Get Trim Variable.
+	 * @return trimmed string
+	 */
+	public function getValidateVariable()
+	{
+		return $this->validateVariable;
+	}
+	
+	
+	//Non Negative Integer Validation
 	public function integerValidation($var)
 	{
-		
+		$this->setValidateVariable($var);
+		if((preg_match('@^[0-9]+$@',$this->getValidateVariable()) !== 1))
+			return false;
+		else 
+			return true;
 	}
 }
